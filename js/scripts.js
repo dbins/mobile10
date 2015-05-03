72c51dba72357c5d3881442fa771f42a79669498
dbins <bins@ig.com.br>
2015-05-03 15:54:23 -0300

    2015-05-03

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+		//http://www.javascriptlint.com/online_lint.php
 		var codigo_produto  = 0;
 		
 		 // alert dialog dismissed
@@ -12,47 +13,47 @@
 
 		function echeck(str) {
 
-			var at="@"
-			var dot="."
-			var lat=str.indexOf(at)
-			var lstr=str.length
-			var ldot=str.indexOf(dot)
+			var at="@";
+			var dot=".";
+			var lat=str.indexOf(at);
+			var lstr=str.length;
+			var ldot=str.indexOf(dot);
 			if (str.indexOf(at)==-1){
-			   //alert("Invalid E-mail ID")
-			   return false
+			   //alert("Invalid E-mail ID");
+			   return false;
 			}
 
 			if (str.indexOf(at)==-1 || str.indexOf(at)==0 || str.indexOf(at)==lstr){
-			   //alert("Invalid E-mail ID")
-			   return false
+			   //alert("Invalid E-mail ID");
+			   return false;
 			}
 
 			if (str.indexOf(dot)==-1 || str.indexOf(dot)==0 || str.indexOf(dot)==lstr){
-				//alert("Invalid E-mail ID")
-				return false
+				//alert("Invalid E-mail ID");
+				return false;
 			}
 
 			 if (str.indexOf(at,(lat+1))!=-1){
-				//alert("Invalid E-mail ID")
-				return false
+				//alert("Invalid E-mail ID");
+				return false;
 			 }
 
 			 if (str.substring(lat-1,lat)==dot || str.substring(lat+1,lat+2)==dot){
-				//alert("Invalid E-mail ID")
-				return false
+				//alert("Invalid E-mail ID");
+				return false;
 			 }
 
 			 if (str.indexOf(dot,(lat+2))==-1){
-				//alert("Invalid E-mail ID")
-				return false
+				//alert("Invalid E-mail ID");
+				return false;
 			 }
 			
 			 if (str.indexOf(" ")!=-1){
-				//alert("Invalid E-mail ID")
-				return false
+				//alert("Invalid E-mail ID")/
+				return false;
 			 }
 
-			 return true					
+			 return true;				
 		}
 		
 		document.addEventListener("deviceready", onDeviceReady, false);
@@ -494,7 +495,7 @@
 		
 		$(document).on('pageinit', '#tela10', function(){ 
 			if ($.mobile.pageData && $.mobile.pageData.codigo){
-				codigo_produto =  $.mobile.pageData.codigo);
+				codigo_produto =  $.mobile.pageData.codigo;
 			}
 		
 			//var parameters = $(this).data("url").split("?")[1];
